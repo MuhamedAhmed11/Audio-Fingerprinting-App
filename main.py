ef8f1f84612027f83488d25642bfd4e046e328d3
Ahmed <goldahmed40@yahoo.com>
2020-04-24 17:49:28 +0200

    equal spectrograms detected

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
-from PyQt5 import QtWidgets, QtGui
-from mainWindow import Ui_MainWindow
-import sys
-import winsound
-from scipy import signal
-from scipy.signal import find_peaks
-import numpy as np
-import matplotlib.pyplot as plt
-import scipy
-from scipy.io import wavfile
-import pyqtgraph as pg
-import os
-from pydub import AudioSegment
 import hashlib
+import os
+import sys
+import wave
+import winsound
+
+import cv2
 import librosa
 import librosa.display
-import wave
+import matplotlib.pyplot as plt
+import numpy as np
 import pylab
-import cv2
+import pyqtgraph as pg
+import scipy
+from pydub import AudioSegment
+from PyQt5 import QtGui, QtWidgets
+from scipy import signal
+from scipy.io import wavfile
+from scipy.signal import find_peaks
 from skimage.feature import peak_local_max
+
+from mainWindow import Ui_MainWindow
 
 
 class ApplicationWindow(QtWidgets.QMainWindow):
@@ -47,6 +49,8 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         self.MIN_HASH_TIME_DELTA = 0
         self.MAX_HASH_TIME_DELTA = 200
         self.DEFAULT_FAN_VALUE = 15
+        self.hashResult1= None
+        self.hashResult2= None
 
         
 
@@ -70,6 +74,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
 
         self.ui.graphicsView.clear()
         self.check_1 = True
+        self.check_2= False
         self.spectrogramFunc()
         print("ESHTAAA")
 
@@ -88,6 +93,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
                 sys.exit
 
         self.check_2 = True
+        self.check_1 = False
         self.spectrogramFunc()
 
     def playSound(self):
@@ -108,48 +114,74 @@ class ApplicationWindow(QtWidgets.QMainWindow):
             winsound.PlaySound(self.filepath2[0], winsound.SND_FILENAME)
             return
 
-    def hash_file(self,peaks,fan_valu):
-        for i in range(len(peaks)):
-            for j in range(1, fan_value):
-                if (i + j) < len(peaks):
+    def hash_file(self,peaks):
+       
+        
+        h1 = hashlib.sha1()
+        h2 = hashlib.sha1()
+      
+        if self.check_1:
+            h1.update(peaks)
+            self.hashResult1 = h1.hexdigest()
+        if self.check_2:
+            h2.update(peaks)
+            self.hashResult2 = h2.hexdigest()
 
-                    freq1 = peaks[i][self.IDX_FREQ_I]
-                    freq2 = peaks[i + j][self.IDX_FREQ_I]
-                    t1 = peaks[i][self.IDX_TIME_J]
-                    t2 = peaks[i + j][self.IDX_TIME_J]
-                    t_delta = t2 - t1
+        if self.hashResult1 and self.hashResult2:
+            if self.hashResult1==self.hashResult2:
+                print("both songs are the same")
+            else:
+                print("two different songs")    
+            #result= int(int(self.hashResult2) - int(self.hashResult1))
+        #     if result < 0:
+        #         result=-result
+        #     if (int(result)/int(hashResult1)) > 0.8:
+        #         print("similar music or vocal")  
+        #     else:
+        #         print("not similar")
+        # print(" na ")                
+        #hashResult2 = h2.hexdigest()
+        # print("first hash:")
+        # print(hashResult1)
+    # print(hashResult2)
+    # print(hashResult1=hashResult2)
+    def misc(self):
+         # for i in range(len(peaks)):
+        #     for j in range(1, fan_value):
+        #         if (i + j) < len(peaks):
 
-                if t_delta >= self.MIN_HASH_TIME_DELTA and t_delta <= self.MAX_HASH_TIME_DELTA:
-                        h = hashlib.sha1(
-                            "%s|%s|%s" % (str(freq1), str(freq2), str(t_delta)))
-                        yield (h.hexdigest()[0:FINGERPRINT_REDUCTION], t1)
+        #             freq1 = peaks[i]#[self.IDX_FREQ_I]
+        #             freq2 = peaks[i + j]#[self.IDX_FREQ_I]
+        #             t1 = peaks[i]#[self.IDX_TIME_J]
+        #             t2 = peaks[i + j]#[self.IDX_TIME_J]
+        #             t_delta = t2 - t1
 
-        # # make a hash object
-        # h1 = hashlib.sha1()
-        # h2 = hashlib.sha1()
-        # # open file for reading in binary mode
-        # with open(self.filepath1[0], 'rb') as file:
-        #     # loop till the end of the file
-        #     chunk1 = 0
-        #     while chunk1 != b'':
-        #         # read only 1024 bytes at a time
-        #         chunk1 = file.read(1024)
-        #         h1.update(chunk1)
-        # # return the hex representation of digest
+        #         if t_delta >= self.MIN_HASH_TIME_DELTA and t_delta <= self.MAX_HASH_TIME_DELTA:
+        #                 h = hashlib.sha1(
+        #                     "%s|%s|%s" % (str(freq1), str(freq2), str(t_delta)))
+        #                 return(h.hexdigest()[0:FINGERPRINT_REDUCTION], t1)
+          # open file for reading in binary mode
+    
+            # loop till the end of the file
+        #chunk1 = str
+        #while chunk1 != b'':
+            
+            # read only 1024 bytes at a time
+            # for y in peaks and j in range(len(peaks)):
+            #     if j < len(peaks/4):
+            #         chunk1=peaks
+            # return the hex representation of digest
         # with open(self.filepath2[0], 'rb') as file:
-        #     # loop till the end of the file
+        # # loop till the end of the file
         #     chunk2 = 0
         #     while chunk2 != b'':
-        #         # read only 1024 bytes at a time
+        #     # read only 1024 bytes at a time
         #         chunk2 = file.read(1024)
         #         h2.update(chunk2)
-        # # return the hex representation of digest
-        # hashResult1 = h1.hexdigest()
-        # hashResult2 = h2.hexdigest()
-        # # print(hashResult1)
-        # # print(hashResult2)
-        # # print(hashResult1=hashResult2)
-    
+        # return the hex representation of digest
+
+        # make a hash object
+        pass
     def get_wav_info(self, wav_file):
         wav = wave.open(wav_file, 'r')
         frames = wav.readframes(-1)
@@ -213,7 +245,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
             ((spectrogramArray)[0])[0]), "--", color="red")
     
         
-        fingerprint=self.hash_file(peaks,self.DEFAULT_FAN_VALUE)
+        fingerprint=self.hash_file(peaks)
         print("fingerprint:")
         print("   ")  
         print(fingerprint)  
